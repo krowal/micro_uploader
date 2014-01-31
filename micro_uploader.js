a9b9813d2f371c3eecfc3e57b60f5616df2caa27
lukasz <lukasz@172.21.90.11>
2014-01-31 10:41:47 +0100

    fixes

--- micro_uploader.js
+++ micro_uploader.js
@@ -7,93 +7,119 @@
 
 (function($){	
 	$.fn.microUploader = function(settings){
-		var T = this;
-		var _files = [];
-		var settings = $.extend({
-			input:null,
-			drop:null,
-			multiple:true,
-			autoStart:false,
-			url:null,
+		//parsing arguments from initialization
+		if(typeof arguments[0] != 'object'){
+			switch(arguments[0]){
+				case 'cancel':
+					$(this).trigger('cancel', arguments[1]);
+					break;
+				case 'startUpload':
+					$(this).trigger('startUpload');
+					break;
+			}
+			return;
+		}else if(typeof settings == 'object'){
+			var settings = $.extend({
+				input:null,
+				drop:null,
+				multiple:true,
+				autoStart:false,
+				url:null,
+
+				//events handlers
+				fileSelected:function(){},			//after file or files are selected
+				fileUploadStart:function(){},		//single file upload start
+				fileUploadProgress:function(){},	//single file upload progress change
+				fileUploadFinish:function(){},		//single file upload finish
+				uploadFinish:function(){}			//all files upload finish
+			}, settings);
 			
-			//events handlers
-			fileSelected:function(){},			//after file or files are selected
-			fileUploadStart:function(){},		//single file upload start
-			fileUploadProgress:function(){},	//single file upload progress change
-			fileUploadFinish:function(){},		//single file upload finish
-			uploadFinish:function(){}			//all files upload finish
-		}, settings);
-		
-		this.selectFiles = function(files){
-			_files = files;
-			settings.fileSelected(_files);
-			if(settings.autoStart) uploader.autoStart();
-		}
-		
-		if(settings.url === null) return console.error('microUpload: no url specified in settings'); //url is required
-		if(settings.input === null) settings.input = $('<input type=file />');
-		if(settings.multiple) settings.input.attr('multiple', 'multiple');
-		
-		var uploader = new (function(){
-			var getFiles = function(){
-				return _files;
-			}
-			this.getFiles = function(){
-				return getFiles();
-			}
-			this.startUpload = function(){
-				var files = this.getFiles();
-				for(var i=0; i<files.length; i++){
-					(function(file, index){
-						var form = new FormData();
-						form.append('file', file);
-						form.append('index', index);
-						var xhr = new XMLHttpRequest();
-						xhr.idd = i;
-						xhr.upload.addEventListener('progress', function(e){
-							if(e.lengthComputable){
-								file.progress = Math.round((e.loaded * 100) / e.total);
-							}
-							settings.fileUploadProgress(file, index);
-						}, false);
-						
-						xhr.onreadystatechange = function(){
-							if (this.readyState == 4){
-								settings.fileUploadFinish(this.responseText, file, index);
-							}
-						}
-						
-						settings.fileUploadStart(file, index);
-						xhr.open("POST", settings.url);
-						xhr.send(form);
-					})(files[i], i);
+			var T = this;
+			var _files = [];
+			var nextId = 0;
+			var xhrs = {};
+			var uploader = new (function(){
+				this.cancelUpload = function(index){
+					if(typeof(xhrs[index]) != 'undefined'){
+						xhrs[index].abort();
+						delete xhrs[index];
+					}
 				}
-			}
-			this.autoStart = function(){
-				this.startUpload();
-			}
-		})();
+				this.startUpload = function(){
+					for(var i=0; i<_files.length; i++){
+						(function(file){
+							var form = new FormData();
+							form.append('file', file);
+							form.append('index', file.id);
+							var xhr = new XMLHttpRequest();
+							xhr.idd = i;
+							xhr.upload.addEventListener('progress', function(e){
+								if(e.lengthComputable){
+									file.progress = Math.round((e.loaded * 100) / e.total);
+								}
+								settings.fileUploadProgress(file, file.id);
+							}, false);
 
+							xhr.onreadystatechange = function(){
+								if (this.readyState == 4){
+									settings.fileUploadFinish(this.responseText, file, file.id);
+									delete xhrs[file.id];
+								}
+							}
 
-		settings.input.change(function(e){
-			T.selectFiles(this.files);
-		});
-		
-		if(settings.drop){
-			settings.drop.on('dragover', function(e){e.preventDefault();});
-			settings.drop.on('dragenter', function(e){e.preventDefault();});
-			settings.drop.on('dragexit', function(e){e.preventDefault();});
-			settings.drop.on('drop', function(e){
-				T.selectFiles(e.originalEvent.dataTransfer.files)
-				e.preventDefault();
+							settings.fileUploadStart(file, file.id);
+							xhr.open("POST", settings.url);
+							xhr.send(form);
+
+							xhrs[file.id] = xhr;
+						})(_files[i]);
+					}
+					console.log(_files)
+					_files = [];
+					console.log(_files)
+				}
+				this.autoStart = function(){
+					this.startUpload();
+				}
+			})();
+			
+			this.selectFiles = function(files){
+				var new_tmp = [];
+				for(var i=0; i<files.length; i++){
+					files[i].id = nextId++;
+					_files.push(files[i]);
+					new_tmp.push(files[i]);
+				}
+				settings.fileSelected(new_tmp);
+				if(settings.autoStart) uploader.autoStart();
+			}
+
+			if(settings.url === null) return console.error('microUpload: no url specified in settings'); //url is required
+			if(settings.input === null) settings.input = $('<input type=file />');
+			if(settings.multiple) settings.input.attr('multiple', 'multiple');
+
+			settings.input.change(function(e){
+				T.selectFiles(this.files);
+			});
+
+			if(settings.drop){
+				settings.drop.on('dragover', function(e){e.preventDefault();});
+				settings.drop.on('dragenter', function(e){e.preventDefault();});
+				settings.drop.on('dragexit', function(e){e.preventDefault();});
+				settings.drop.on('drop', function(e){
+					T.selectFiles(e.originalEvent.dataTransfer.files)
+					e.preventDefault();
+				});
+			}
+
+			$(this).click(function(){
+				settings.input.click();
+				return false;
+			}).on('cancel', function(){
+				uploader.cancelUpload(arguments[1]);
+			}).on('startUpload', function(){
+				uploader.startUpload();
 			});
 		}
-		
-		$(this).click(function(){
-			settings.input.click();
-			return false;
-		}).on('startUpload', function(){
-			uploader.startUpload();
-		});
 	}
 })(jQuery);
